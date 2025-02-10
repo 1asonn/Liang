344cbebe84179f4c739e49408fcd6bcfe48c4311
1asonn <2554195251@qq.com>
2025-02-10 22:12:35 +0800

    first commit

---
+++ vueadmin-vue/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <!-- <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav> -->
+    <router-view/>
+  </div>
+</template>
+
+
+
+<script>
+  export default {
+    name:'App',
+    watch:{
+      /* 页面刷新后保持选中的标签页 */
+      $route(to,from){
+        if(to.path !='/login'){
+          let obj = {
+            name  : to.name,
+            title : to.meta.title
+          }
+
+          this.$store.commit("addTab",obj)
+        }
+      }
+
+    }
+  }
+</script>
+
+
+
+<style>
+/* 设置外部页面禁止滑动 */
+html{
+  overflow: hidden;
+}
+p{
+  font-size: 14px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
